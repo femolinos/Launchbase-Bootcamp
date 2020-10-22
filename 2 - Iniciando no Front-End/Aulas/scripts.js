67d45de6947bbe7681061090b16ef7e3fa4dc3da
= <felipemolinos01@gmail.com>
2020-10-21 21:58:08 -0300

    Recomeço do curso Launchbase

--- 2 - Iniciando no Front-End/Aulas/scripts.js
+++ 2 - Iniciando no Front-End/Aulas/scripts.js
@@ -1,15 +1,16 @@
-const modalOverlay = document.querySelector('.modal-overlay'); /* Seleciona elementos da tela baseado na classe CSS */
+const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
-for(let card of cards){
-  card.addEventListener("click", function(){
-    const video_id = card.getAttribute("id");
+for(let card of cards) {
+  card.addEventListener('click', function(){
+    const videoId = card.getAttribute("id");
     modalOverlay.classList.add('active');
-    modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${video_id}`;
+    console.log(modalOverlay.querySelector('iframe'));
+    modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`;
   });
 }
 
-document.querySelector('.close-modal').addEventListener("click", function(){
+document.querySelector('.close-modal').addEventListener('click', function() {
   modalOverlay.classList.remove('active');
-  modalOverlay.querySelector("iframe").src = "";
+  modalOverlay.querySelector('iframe').src = "";
 });
